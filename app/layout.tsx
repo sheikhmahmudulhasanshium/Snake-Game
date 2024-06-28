880d23cb5cf806a924d081e46bdab754d8032c23
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-06-29 02:27:44 +0600

    minor update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icons" href="/favicon.ico"/>
+      </head>
       <body className={font.className}>{children}</body>
     </html>
   );
